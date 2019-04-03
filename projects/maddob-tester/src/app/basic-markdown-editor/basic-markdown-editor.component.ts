d078a397d42308583e04dfb31ee541d44cf57dbe
martin.dobrev@noser.com <LEviZU87>
2019-04-03 11:19:43 +0300

    Step 2: install codemirror & marked deps, basic editor stucture, init codemirror

--- projects/maddob-tester/src/app/basic-markdown-editor/basic-markdown-editor.component.ts
+++ projects/maddob-tester/src/app/basic-markdown-editor/basic-markdown-editor.component.ts
@@ -1,14 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+
+declare var CodeMirror: any;
+declare var marked: any;
 
 @Component({
   selector: 'app-basic-markdown-editor',
   templateUrl: './basic-markdown-editor.component.html',
   styleUrls: ['./basic-markdown-editor.component.css']
 })
-export class BasicMarkdownEditorComponent implements OnInit {
+export class BasicMarkdownEditorComponent implements OnInit, AfterViewInit {
+
+
+  @ViewChild("editor") editor: ElementRef;
 
   constructor() { }
 
   ngOnInit() {}
 
+  ngAfterViewInit() {
+    // initialize code mirror instance
+    CodeMirror.fromTextArea(this.editor.nativeElement, {
+      mode: 'markdown',
+      lineNumbers: true
+    });
+  }
 }
